2d39e204ce5d9a59e0b66544575285fa0ef07ba9
woolson <woolson.lee@gmail.com>
2018-02-06 19:22:53 +0800

    展示

--- src/components/shows.vue
+++ src/components/shows.vue
@@ -1,104 +1,96 @@
 <template>
-  <div
-    class="shows-page"
-
-  >
-    <div class="backgroundImage">
-      展示
-      <!-- <img :src="img2"  class="animated fadeInDownBig ">
-      <img :src="img3"  class="animated swing infinite">
-      <img :src="img6" class="animated slideInRight ">
-      <img :src="img7" >
-      <img :src="img8" >
-      <img :src="img9" class="animated jello ">
-      <img :src="img10" class="animated rotateIn "> -->
-    </div>
-  </div>
+	<div
+		class="shows-page"
+	>
+		<div class="titleHead">
+			<img :src="img1" >
+		</div>
+		<!-- <div class="showList">
+			<ul>
+				<li>
+					<img :src="img4" >
+				</li>
+				<li>
+					<img :src="img5" >
+				</li>
+				<li>
+					<img :src="img6" >
+				</li>
+				<li>
+					<img :src="img7" >
+				</li>
+				<li>
+					<img :src="img8" >
+				</li>
+				<li>
+					<img :src="img9" >
+				</li>
+				<li>
+					<img :src="img10" >
+				</li>
+				<li>
+					<img :src="img11" >
+				</li>
+				<li>
+					<img :src="img12" >
+				</li>
+				<li>
+					<img :src="img13" >
+				</li>
+			</ul>
+		</div>
+		<div class="footer_btn">
+			<img :src="img2" >
+			<img :src="img3" >
+		</div> -->
+	</div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        img2: require('@Images/1_02.png'),
-        img3: require('@Images/1_03.png'),
-        img6: require('@Images/1_06.png'),
-        img7: require('@Images/1_07.png'),
-        img8: require('@Images/1_08.png'),
-        img9: require('@Images/1_09.png'),
-        img10: require('@Images/1_10.png'),
-        move: false
-      }
-    },
-    methods: {
-      startMove () {
-        this.move = true
-        console.log("1ture")
-      }
-    }
-  }
+	export default {
+		data () {
+			return {
+				img1: require('@Images/6-1.png'),
+				img2: require('@Images/6-2.png'),
+				img3: require('@Images/6-3.png'),
+				img4: require('@Images/6-4.png'),
+				img5: require('@Images/6-5.png'),
+				img6: require('@Images/6-6.png'),
+				img7: require('@Images/6-7.png'),
+				img8: require('@Images/6-8.png'),
+				img9: require('@Images/6-9.png'),
+				img10: require('@Images/6-10.png'),
+				img11: require('@Images/6-11.png'),
+				img12: require('@Images/6-12.png'),
+				img13: require('@Images/6-13.png'),
+				move: false
+			}
+		},
+		methods: {
+			startMove () {
+				this.move = true
+				console.log("1ture")
+			}
+		}
+	}
 </script>
 
 <style lang="stylus">
-  .shows-page
-    width 100vw
-    height 100vh
-    display flex
-    flex-direction column
-    justify-content space-around
-    z-index 6
-    background-image url('/static/images/1_01.png')
-    background-repeat no-repeat
-    background-size cover
-    position absolute
-    .backgroundImage
-      position relative
-      width 100vw
-      height 100vh
-      overflow hidden
-      img:nth-of-type(1)
-        position absolute
-        top 20%
-        left 20%
-        width 70%
-      img:nth-of-type(2)
-        position absolute
-        top 5vh
-        left 10%
-        width 10%
-      img:nth-of-type(3)
-        position absolute
-        top 55%
-        right 15%
-        width 10%
-      img:nth-of-type(4)
-        position absolute
-        bottom 24%
-        left 20%
-        width 8%
-      img:nth-of-type(5)
-        position absolute
-        bottom 18%
-        right 17%
-        width 8%
-      img:nth-of-type(6)
-        position absolute
-        bottom 20%
-        right 40%
-        width 18%
-        z-index 8
-      img:nth-of-type(7)
-        position absolute
-        bottom -20%
-        right 10%
-        width 80%
-  .move
-    animation moveUp 0.5s forwards
-  @keyframes moveUp
-    0% {
-      transform translate(0, 0)
-    }
-    100% {
-      transform translate(0, 100%)
-    }
+	.shows-page
+		width 100vw
+		height 100vh
+		display flex
+		flex-direction column
+		justify-content space-around
+		z-index 6
+		background-image url('/static/images/1_01.png')
+		background-repeat no-repeat
+		background-size cover
+		position absolute
+		.titleHead
+			img
+				position absolute
+				top 8%
+				left 30%
+				width 40%
 </style>
